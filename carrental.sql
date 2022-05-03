34b31987ed0253a5b3b0467b50e1e90d4a0bcc5e
lucemorato <63071872+lucemorato@users.noreply.github.com>
2022-05-03 18:16:42 +0200

    baza-v3
    s profesorom

--- carrental.sql
+++ carrental.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 14, 2022 at 05:45 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.6
+-- Generation Time: May 03, 2022 at 06:15 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -39,11 +40,39 @@ CREATE TABLE `kupci` (
 --
 
 INSERT INTO `kupci` (`Id`, `Ime`, `Prezime`, `Email`) VALUES
-(1, 'Lucija', 'Mikulic', 'lucija.mikulic@gmail.com'),
-(2, 'Karlo', 'Rajić', 'rajic@yahoo.com'),
-(3, 'Vlado', 'Trešnja', 'vlada@gmail.com'),
-(4, 'Darija', 'Huljić', 'hulja@aspira.hr'),
-(5, 'Mislav', 'Markić', 'marko@inet.hr');
+(143, 'Karla', 'Mikulic', 'lucija.mikulic@live.com'),
+(144, 'Marija', 'Duplić', 'marija@inet.hr'),
+(145, 'Matej', 'Laurić', 'laura@yahoo.com'),
+(146, 'Dragutin', 'Perković', 'dragulj@hotmail.com'),
+(169, 'Linda', 'Prelijepa', 'linda@gmail.com'),
+(182, 'Molim', 'Lijepo', 'radipliz@net.hr'),
+(185, 'Molim', 'Te', 'boze@gmail.com'),
+(186, 'Honda', 'safd', 'ant@outlook.com'),
+(187, 'Vozilo', 'hes', 'y@gmail.com'),
+(188, 'Honda', 'hgfd', 'gladna.je@gmail.com'),
+(189, 'Lucija', 'Mikulic', 'lucija@gmail.com'),
+(190, 'Lucija', 'Vjerkić', 'tg@g.l'),
+(191, 'Sandra', 'sadf', 'hjoj@gmai.c'),
+(193, 'Karla', 'Mikulic', 'erg@g.k'),
+(194, 'Ante', 'Automobil', 'aut@gmail.com'),
+(196, 'Marijana', 'Petrov', 'marre@gmail.com'),
+(0, 'lucija', 'lucic', 'lu@gmail.com'),
+(0, 'lucija', 'lucic', 'lu@gmail.com'),
+(0, 'lucija', 'lucic', 'd@gmail.com'),
+(0, 'Gabi', 'Rako', 'ra@gmail.com'),
+(0, 'Gabi', 'Rako', 'ra@gmail.com'),
+(0, 'Gabi', 'Rako', 'heh@gmail.com'),
+(0, 'Gabi', 'TrutaniÄ‡', 'heh@gmail.com'),
+(0, 'lucija', 'lucic', 'tvukman@aspira.hr'),
+(0, 'lucija', 'VDSD', 'lculic@aspira.hr'),
+(0, 'lucija', 'VDSD', 'lculic@aspira.hr'),
+(0, 'lucija', 'VDSD', 'lculic@aspira.hr'),
+(0, 'lucija', 'VDSD', 'lculic@aspira.hr'),
+(0, 'lucija', 'VDSD', 'lculic@aspira.hr'),
+(0, 'lucija', 'VDSD', 'lculic@aspira.hr'),
+(0, 'lucija', 'VDSD', 'lculic@aspira.hr'),
+(0, 'lucija', 'VDSD', 'lculic@aspira.hr'),
+(0, 'lucija', 'VDSD', 'd@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -56,8 +85,47 @@ CREATE TABLE `najmovi` (
   `VoziloID` int(10) NOT NULL,
   `KupacID` int(10) NOT NULL,
   `BrojDana` int(10) NOT NULL,
-  `UkupnaZarada` int(255) DEFAULT NULL
+  `Total` int(255) NOT NULL,
+  `Status` varchar(50) NOT NULL DEFAULT 'Deactivated',
+  `Cijena` int(11) NOT NULL,
+  `ime` varchar(50) DEFAULT NULL,
+  `prezime` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `imeVozila` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `najmovi`
+--
+
+INSERT INTO `najmovi` (`Id`, `VoziloID`, `KupacID`, `BrojDana`, `Total`, `Status`, `Cijena`, `ime`, `prezime`, `email`, `imeVozila`) VALUES
+(72, 1, 143, 8, 25600, 'Activated', 0, NULL, NULL, NULL, NULL),
+(73, 55, 144, 3, 1950, 'Activated', 0, NULL, NULL, NULL, NULL),
+(74, 5, 145, 7, 560, 'Activated', 0, NULL, NULL, NULL, NULL),
+(75, 2, 146, 2, 460, 'Deactivated', 0, NULL, NULL, NULL, NULL),
+(98, 4, 169, 6, 660, 'Deactivated', 0, NULL, NULL, NULL, NULL),
+(111, 6, 182, 6, 240, 'Activated', 0, NULL, NULL, NULL, NULL),
+(114, 1, 185, 7, 0, 'Activated', 0, NULL, NULL, NULL, NULL),
+(115, 1, 186, 4, 0, 'Activated', 0, NULL, NULL, NULL, NULL),
+(116, 1, 187, 5, 0, 'Activated', 0, NULL, NULL, NULL, NULL),
+(117, 1, 188, 5, 0, 'Activated', 0, NULL, NULL, NULL, NULL),
+(118, 1, 189, 2, 0, 'Activated', 0, NULL, NULL, NULL, NULL),
+(119, 2, 190, 2, 0, 'Deactivated', 0, NULL, NULL, NULL, NULL),
+(120, 4, 191, 2, 0, 'Deactivated', 0, NULL, NULL, NULL, NULL),
+(122, 1, 193, 2, 0, 'Activated', 0, NULL, NULL, NULL, NULL),
+(123, 1, 194, 2, 0, 'Activated', 0, NULL, NULL, NULL, NULL),
+(125, 2, 196, 7, 0, 'Deactivated', 0, NULL, NULL, NULL, NULL),
+(126, 55, 0, 8, 5200, 'Activated', 650, 'Gabi', 'TrutaniÄ‡', 'heh@gmail.com', 'BMW'),
+(127, 5, 0, 5, 400, 'Activated', 80, 'lucija', 'lucic', 'tvukman@aspira.hr', NULL),
+(128, 55, 0, 4, 320, 'Activated', 80, 'lucija', 'VDSD', 'lculic@aspira.hr', NULL),
+(129, 55, 0, 4, 320, 'Activated', 80, 'lucija', 'VDSD', 'lculic@aspira.hr', ''),
+(130, 55, 0, 4, 320, 'Activated', 80, 'lucija', 'VDSD', 'lculic@aspira.hr', ''),
+(131, 55, 0, 4, 320, 'Activated', 80, 'lucija', 'VDSD', 'lculic@aspira.hr', ''),
+(132, 55, 0, 4, 320, 'Activated', 80, 'lucija', 'VDSD', 'lculic@aspira.hr', ''),
+(133, 55, 0, 4, 320, 'Activated', 80, 'lucija', 'VDSD', 'lculic@aspira.hr', NULL),
+(134, 55, 0, 4, 320, 'Activated', 80, 'lucija', 'VDSD', 'lculic@aspira.hr', NULL),
+(135, 55, 0, 4, 320, 'Activated', 80, 'lucija', 'VDSD', 'lculic@aspira.hr', NULL),
+(136, 2, 0, 9, 720, 'Deactivated', 80, 'lucija', 'VDSD', 'd@gmail.com', NULL);
 
 -- --------------------------------------------------------
 
@@ -70,82 +138,42 @@ CREATE TABLE `vozila` (
   `Ime` varchar(255) NOT NULL,
   `Cijena` int(11) NOT NULL,
   `Kategorija` varchar(255) NOT NULL,
-  `Kolicina` int(100) NOT NULL
+  `Kolicina` int(100) NOT NULL,
+  `Oznaka` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `vozila`
 --
 
-INSERT INTO `vozila` (`Id`, `Ime`, `Cijena`, `Kategorija`, `Kolicina`) VALUES
-(1, 'Tesla', 3200, 'Automobil', 8),
-(2, 'Škoda', 230, 'Automobil', 21),
-(4, 'Vespa', 110, 'Skuter', 3),
-(5, 'Yamaha', 80, 'Skuter', 43),
-(6, 'BTWIN', 40, 'Bicikl', 16),
-(7, 'Rockrider', 60, 'Bicikl', 9),
-(55, 'BMW', 650, 'Automobil', 2);
+INSERT INTO `vozila` (`Id`, `Ime`, `Cijena`, `Kategorija`, `Kolicina`, `Oznaka`) VALUES
+(1, 'Tesla', 3200, 'Automobil', 0, 0),
+(2, 'Škoda', 230, 'Automobil', 14, 0),
+(4, 'Vespa', 110, 'Skuter', 8, 0),
+(5, 'Yamaha', 80, 'Skuter', 38, 0),
+(6, 'BTWIN', 40, 'Bicikl', 12, 0),
+(7, 'Rockrider', 60, 'Bicikl', 12, 0),
+(55, 'BMW', 650, 'Automobil', -4, 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `kupci`
---
-ALTER TABLE `kupci`
-  ADD PRIMARY KEY (`Id`),
-  ADD KEY `Id` (`Id`);
-
---
 -- Indexes for table `najmovi`
 --
 ALTER TABLE `najmovi`
-  ADD PRIMARY KEY (`Id`),
-  ADD KEY `Id` (`Id`),
-  ADD KEY `Id_2` (`Id`),
-  ADD KEY `test` (`KupacID`),
-  ADD KEY `test2` (`VoziloID`);
-
---
--- Indexes for table `vozila`
---
-ALTER TABLE `vozila`
-  ADD PRIMARY KEY (`Id`),
-  ADD KEY `Id` (`Id`);
+  ADD PRIMARY KEY (`Id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `kupci`
---
-ALTER TABLE `kupci`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
 -- AUTO_INCREMENT for table `najmovi`
 --
 ALTER TABLE `najmovi`
-  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `vozila`
---
-ALTER TABLE `vozila`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `najmovi`
---
-ALTER TABLE `najmovi`
-  ADD CONSTRAINT `test` FOREIGN KEY (`KupacID`) REFERENCES `kupci` (`Id`),
-  ADD CONSTRAINT `test2` FOREIGN KEY (`VoziloID`) REFERENCES `vozila` (`Id`);
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=137;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
