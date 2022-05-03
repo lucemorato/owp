9ac54356d398bd831e7201f607a72fb27452870b
lucemorato <63071872+lucemorato@users.noreply.github.com>
2022-05-03 18:38:30 +0200

    skoro sve gotovo

--- rentacar/zaprimi.php
+++ rentacar/zaprimi.php
@@ -1,21 +1,64 @@
+<?php include('includes/header.php'); ?>
+
+<?php
+    $db = mysqli_connect("localhost", "root", "", "carrental");
+
+?>
+
+
+<?php
+    //provjeri je li car id setiran ili ne
+    if(isset($_GET['id']))
+    {
+        
+        $car_id = $_GET['id'];
+
+    
+        $sql = "UPDATE  vozila
+            SET kolicina = kolicina + 1,
+                status = 'Deactivated'
+            WHERE Id = $car_id";
+        $db->query($sql);
+
+}
+
+      
+
+        //dohvati detlje selektiranog vozila
+        /*$sql = "SELECT * FROM najmovi WHERE VoziloID=$car_id";
+        $res = mysqli_query($db, $sql);
+        $count = mysqli_num_rows($res);
+
+        if($count == 1)
+        {
+            $row = mysqli_fetch_assoc($res);
+            
+            $status = $row['Status'];
+        }
+        else
+        {
+            header('location:'.SITEURL);
+        }*/
+
+
+    
+ header('location: ./administracija.php');
+?>
+
 <?php
 
-$id = $_GET['id'];
 
-$dbname = "carrental";
-$db = mysqli_connect("localhost", "root", "", $dbname);
-if (!$db) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+$sql4 = "UPDATE vozila SET kolicina = kolicina + 1 WHERE Id = $car_id";
+$db->query($sql4);
+$sql5 = "UPDATE najmovi SET status = 'Deactivated' WHERE VoziloID = $car_id";
+$db->query($sql5);
 
 
-$sql = "UPDATE vozila SET kolicina = kolicina + 1 WHERE Id = $id";
-
-if (mysqli_query($db, $sql)) {
-    mysqli_close($db);
-    header('Location: administracija.php'); 
-    exit;
-} else {
-    echo "Error updating record";
-}
+// if (mysqli_query($db, $sql)) {
+//     mysqli_close($db);
+//     header('Location: administracija.php'); 
+//     exit;
+// } else {
+//     echo "Error updating record";
+// }
 
